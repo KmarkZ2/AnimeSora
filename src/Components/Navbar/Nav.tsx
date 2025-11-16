ad438889413cc300ba4f3ea5a677a6fb371a3dba
mark <markkm68@gmail.com>
2025-11-16 19:22:14 +0200

    Navbar refracted

--- src/Components/Navbar/Nav.tsx
+++ src/Components/Navbar/Nav.tsx
@@ -1,45 +1,17 @@
-"use client";
-
-import clsx from "clsx";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
-import { useState } from "react";
-
-const Links = {
-  navbar: [
-    { href: "/", label: "News" },
-    { href: "/genre", label: "Genre" },
-    { href: "/random", label: "Random" },
-  ],
-  profile: [
-    { href: "/profile", label: "Profile" },
-    { href: "/addNews", label: "Add News" },
-  ],
-};
+import NavLinks from "./NavLinks";
 
 export default function Nav() {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const path = usePathname();
   return (
     <nav className="flex sticky top-[0] z-[5] items-center md:h-[105px] h-[75px] justify-between pl-[5px] pr-[5px] md:pl-[50px] md:pr-[50px] bg-[#121212d9] border-[rgba(0,224,255,0.2)] border-[1px] shadow-[0_0_20px_0_rgba(255,79,216,0.03),_0_0_70px_0_rgba(255,79,216,0.2)] rounded-b-[10px]">
-      <h2 className="text-[#E0E0E0] font-[Orbitron] font-bold md:text-[1.75rem] text-[1rem]">
-        AnimeSora
-      </h2>
-      <div className="flex md:gap-[50px] gap-[10px] items-center justify-center">
-        {Links.navbar.map((el) => (
-          <Link
-            href={el.href}
-            key={el.label}
-            className={`md:text-[1.5rem] text-[0.75rem] font-[Orbitron] font-bold hover:text-[#FFFFFF] transition-[200ms] ${
-              path === el.href ? "text-[#FFD500]" : "text-[#B0B0B0]"
-            }`}
-          >
-            {el.label}
-          </Link>
-        ))}
-      </div>
-
+      <Link href={"/"} className="flex-1">
+        <h2 className="text-[#E0E0E0] font-[Orbitron] font-bold md:text-[1.75rem] text-[1rem]">
+          AnimeSora
+        </h2>
+      </Link>
+      <NavLinks className="flex-2" />
+      <div className="flex-1">
+        {/* 
       <div className="w-auto relative">
         <button
           type="button"
@@ -87,6 +59,7 @@ export default function Nav() {
             ))}
           </div>
         </div>
+      </div> */}
       </div>
     </nav>
   );
---
+++ src/Components/Navbar/NavLinks.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const Links = {
+  navbar: [
+    { href: "/", label: "News" },
+    { href: "/genre", label: "Genre" },
+    { href: "/random", label: "Random" },
+  ],
+  profile: [
+    { href: "/profile", label: "Profile" },
+    { href: "/addNews", label: "Add News" },
+  ],
+};
+
+type NavLinksProps = {
+  className?: string;
+};
+
+export default function NavLinks({ className }: NavLinksProps) {
+  const path = usePathname();
+
+  return (
+    <div
+      className={
+        "flex md:gap-[50px] gap-[10px] items-center justify-center " + className
+      }
+    >
+      {Links.navbar.map((el) => (
+        <Link
+          href={el.href}
+          key={el.label}
+          className={`md:text-[1.5rem] text-[0.75rem] font-[Orbitron] font-bold hover:text-[#FFFFFF] transition-[200ms] ${
+            path === el.href ? "text-[#FFD500]" : "text-[#B0B0B0]"
+          }`}
+        >
+          {el.label}
+        </Link>
+      ))}
+    </div>
+  );
+}
